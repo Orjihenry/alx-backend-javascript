d211baf54ee377b1854c41508d319cff8ec36f56
Orjihenry <orjihen@yahoo.com>
2023-11-29 12:32:27 +0100

    Parameter defaults ❤️❤️❤️

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,7 +1,3 @@
-export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
-  initialNumber = initialNumber ?? 0;
-  expansion1989 = expansion1989 ?? 89;
-  expansion2019 = expansion2019 ?? 19;
-
+export default function getSumOfHoods(initialNumber = 0, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
